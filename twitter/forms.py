39c02c1e3cfc49dd72136f9ea51201e428ed0091
sandeshpd <sandeshdha26@gmail.com>
2024-03-29 09:48:16 +0530

    specifying web forms

---
+++ twitter/forms.py
@@ -0,0 +1,173 @@
+from django import forms
+from .models import Tweet, Profile, Comment
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+# Upload Profile Picture
+class ProfilePicForm(forms.ModelForm):
+    profile_picture = forms.ImageField(label = 'Profile Picture')
+    profile_bio = forms.CharField(label = 'Profile Bio',
+                                  required = False,
+                                  widget=forms.widgets.Textarea(
+                                      attrs={
+                                          'placeholder': 'Enter your bio to show who you are to your friends.',
+                                          'class': 'form-control',
+                                      }
+                                  ))
+    website_link = forms.CharField(label = 'Your Website URL',
+                                   required = False,
+                                   widget=forms.widgets.TextInput(
+                                      attrs={
+                                          'placeholder': 'Link to your personal website',
+                                          'class': 'form-control',
+                                      }
+                                  ))
+    facebook_link = forms.CharField(label = 'Facebook',
+                                    required = False,
+                                   widget=forms.widgets.TextInput(
+                                      attrs={
+                                          'placeholder': 'Your Facebook Profile Link',
+                                          'class': 'form-control',
+                                      }
+                                  ))
+    instagram_link = forms.CharField(label = 'Instagram',
+                                     required = False,
+                                   widget=forms.widgets.TextInput(
+                                      attrs={
+                                          'placeholder': 'Your Instagram Profile Link',
+                                          'class': 'form-control',
+                                      }
+                                  ))
+    linkedin_link = forms.CharField(label = 'LinkedIn',
+                                    required=False,
+                                    widget=forms.widgets.TextInput(
+                                    attrs={
+                                        'placeholder': 'Your LinkedIn Profile Link',
+                                        'class': 'form-control',
+                                    }
+                                ))
+
+    class Meta:
+        model = Profile
+        fields = ('profile_picture', 'profile_bio', 'website_link', 'facebook_link', 'instagram_link', 'linkedin_link',)
+
+# User Tweeting form
+class TweetForm(forms.ModelForm):
+    body = forms.CharField(required=True,
+                           widget=forms.widgets.Textarea(
+                               attrs={
+                                   'placeholder': 'What is happening?!',
+                                   'class': 'form-control',
+                                   'style': 'height: 150px; border-color: #484848;',
+                               }
+                           ),
+                           label='',
+                        )
+    class Meta:
+        model = Tweet
+        exclude = ('user','likes','bookmarks','comment_count',)
+
+class CommentForm(forms.ModelForm):
+    body = forms.CharField(required=True,
+                              widget=forms.widgets.Textarea(
+                                  attrs={
+                                      'placeholder':'Post your reply...',
+                                      'class':'form-control',
+                                      'style':'height: 150px; border-color: #484848;',
+                                  }
+                              ),
+                              label=''
+                            )
+    class Meta:
+        model = Comment
+        fields = ['body',]
+
+    def save(self,commit = True):
+        instance = super(CommentForm, self).save(commit = False)
+        if commit:
+            instance.save()
+        self.cleaned_data['body'] = ''
+        return instance
+
+# User Signup form
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label='', 
+                             widget=forms.TextInput(
+                                 attrs={
+                                     'class':'form-control',
+                                     'placeholder':'Your Valid Email Address',
+                                 }))
+    first_name = forms.CharField(label='',
+                                 max_length=50,
+                                 widget=forms.TextInput(
+                                     attrs={
+                                         'class':'form-control',
+                                         'placeholder':'Your First Name',
+                                     }))
+    last_name = forms.CharField(label='',
+                                 max_length=60,
+                                 widget=forms.TextInput(
+                                     attrs={
+                                         'class':'form-control',
+                                         'placeholder':'Your Last Name',
+                                     }))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Your User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Enter a strong Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+# User Update Form
+class UpdateUserForm(UserChangeForm):
+    email = forms.EmailField(label='Email', 
+                             widget=forms.TextInput(
+                                 attrs={
+                                     'class':'form-control',
+                                     'placeholder':'Your Valid Email Address',
+                                 }))
+    first_name = forms.CharField(label='First Name',
+                                 max_length=50,
+                                 widget=forms.TextInput(
+                                     attrs={
+                                         'class':'form-control',
+                                         'placeholder':'Your First Name',
+                                     }))
+    last_name = forms.CharField(label='Last Name',
+                                 max_length=60,
+                                 widget=forms.TextInput(
+                                     attrs={
+                                         'class':'form-control',
+                                         'placeholder':'Your Last Name',
+                                     }))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+    
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Your User Name'
+        self.fields['username'].label = 'Username'
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+        self.fields['password'].label = ''
+        self.fields['password'].help_text = "<p>If you want to update the password, <a href={% url 'change_password'%}>click here</a>.</p>"
