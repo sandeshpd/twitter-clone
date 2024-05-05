7e947f0a7e0f388408f07c62dc1e78305d6e5d33
sandeshpd <sandeshdha26@gmail.com>
2024-05-05 12:38:17 +0530

    some changes in URL paths

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('profile_list/', views.profile_list, name='profile_list'),
     path('profile/<int:pk>', views.profile, name='profile'),
     path('search_user/', views.search_user, name='search_user'),
     path('profile/followers/<int:pk>', views.followers, name='followers'),
     path('profile/follows/<int:pk>', views.follows, name='follows'),
-    path('login/', views.login_user, name='login'),
+    path('', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
     path('signup/', views.signup_user, name='signup'),
     path('update/', views.update_user, name='update'),
+    path('update/change_password/', views.change_password, name='change_password'),
     path('tweet_like/<int:pk>', views.tweet_like, name='tweet_like'),
     path('tweet/<int:pk>/comment/', views.tweet_comment, name='tweet_comment'),
     path('tweet_share/<int:pk>', views.tweet_share, name='tweet_share'),
