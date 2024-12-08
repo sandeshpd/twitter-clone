b691912d1cbb19b99c4fbc4768ef992966ab0f78
Sandesh <sandeshdha26@gmail.com>
2024-12-08 14:27:41 +0530

    Modified input field styles

--- twitter/forms.py
+++ twitter/forms.py
@@ -93,70 +93,9 @@ class CommentForm(forms.ModelForm):
         self.cleaned_data['body'] = ''
         return instance
 
-# TODO: Add placeholder instead of label
-# class MDOutlinedTextField(forms.Widget):
-#     template_name = 'md_outlined_text_field.html'
-
-#     def render(self, name, value, attrs=None, renderer=None):
-#         html = f'''
-#         <md-outlined-text-field placeholder="{attrs.get('placeholder', '')}">
-#             <input id="{attrs.get('id', name)}" name="{name}" value="{value or ''}" type="{attrs.get('type', 'text')}">
-#         </md-outlined-text-field>
-#         '''
-#         return mark_safe(html)
-
 # User Signup form
-# FIXME: User entered data do not get stored in DB and SignUp page keep redirecting to itself
+# FIXME: User entered data do not get stored in DB and SignUp page keeps redirecting to itself
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(
-        label='First Name',
-        max_length = 20,
-        widget = forms.TextInput(
-            attrs = {
-                'class': 'md-text-field__input',
-            }
-        )
-    )
-
-    last_name = forms.CharField(
-        label='Last Name',
-        max_length = 30,
-        widget = forms.TextInput(
-            attrs = {
-                'class': 'md-text-field__input',
-            }
-        )
-    )
-
-    email = forms.EmailField(
-        label='Enter Email',
-        widget = forms.EmailInput(
-            attrs = {
-                'class': 'md-text-field__input',
-            }
-        )
-    )
-
-    # TODO: Define password1 and password2 here, not in class Meta
-    
-    password1 = forms.CharField(
-        label = 'Enter Password',
-        widget = forms.widgets.PasswordInput(
-            attrs = {
-                'class': 'md-text-field__input',
-            }
-        )
-    )
-
-    password2 = forms.CharField(
-        label = 'Enter password again to confirm',
-        widget = forms.widgets.PasswordInput(
-            attrs = {
-                'class': 'md-text-field__input',
-            }
-        )
-    )
-    
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
@@ -164,85 +103,47 @@ class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget = forms.TextInput(
-            # label = 'Enter username',
-            attrs = {
-                'class': 'form-control',
-                'placeholder': 'Your Username'
-            }
-        )
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
-
-        self.fields['password1'].widget = forms.PasswordInput(
-            # label='Enter your password',
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Your Password',
-                'type': 'password'
+        self.fields['first_name'].widget.attrs.update(
+            {
+                'class': 'md-text-field__input',
+                'id': 'first_name',
             }
         )
 
-        
-        self.fields['password2'].widget = forms.PasswordInput(
-            # label='Confirm your password',
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Confirm Password',
-                'type': 'password'
+        self.fields['last_name'].widget.attrs.update(
+            {
+                'class': 'md-text-field__input',
+                'id': 'last_name',
             }
         )
 
-        # self.fields['password2'].widget.attrs['class'] = 'md-text-field__input'
-        # self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
-        # self.fields['password2'].label = 'Confirm your password'
-        # self.fields['password1'].type = 'password'
+        self.fields['username'].widget.attrs.update(
+            {
+                'class': 'md-text-field__input',
+                'id': 'username',
+            }
+        )
 
+        self.fields['email'].widget.attrs.update(
+            {
+                'class': 'md-text-field__input',
+                'id': 'email',
+            }
+        )
 
-# class SignUpForm(UserCreationForm):
-#     email = forms.EmailField(
-#         label='', 
-#         widget=forms.TextInput(
-#         attrs={
-#             'class':'md-outlined-text-field',
-#             'placeholder':'Your Valid Email Address',
-# }))
-#     first_name = forms.CharField(
-#         label='',
-#         max_length=50,
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class':'md-outlined-text-field',
-#                 'placeholder':'Your First Name',
-# }))
-#     last_name = forms.CharField(
-#         label='',
-#         max_length=60,
-#         widget=forms.TextInput(
-#                         attrs={
-#                         'class':'md-outlined-text-field',
-#                         'placeholder':'Your Last Name',
-# }))
+        self.fields['password1'].widget.attrs.update(
+            {
+                'class': 'md-text-field__input',
+                'id': 'password1',
+            }
+        )
 
-#     class Meta:
-#             model = User
-#             fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
-
-#         def __init__(self, *args, **kwargs):
-#             super(SignUpForm, self).__init__(*args, **kwargs)
-#             self.fields['username'].widget.attrs['class'] = 'form-control'
-#             self.fields['username'].widget.attrs['placeholder'] = 'Your User Name'
-#             self.fields['username'].label = ''
-#             self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
-
-#             self.fields['password1'].widget.attrs['class'] = 'form-control'
-#             self.fields['password1'].widget.attrs['placeholder'] = 'Enter a strong Password'
-#             self.fields['password1'].label = ''
-#             self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
-
-#             self.fields['password2'].widget.attrs['class'] = 'form-control'
-#             self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-#             self.fields['password2'].label = ''
-#             self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+        self.fields['password2'].widget.attrs.update(
+            {
+                'class': 'md-text-field__input',
+                'id': 'password2',
+            }
+        )
 
 
 # User Update Form
